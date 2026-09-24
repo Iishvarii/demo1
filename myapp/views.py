f2459e8266d86a7e283ed10e15a61d6a89a3afe5
Iishvarii <iishanya0368@gmail.com>
2026-09-24 11:09:19 +0800

    B-branch bweb finish

--- myapp/views.py
+++ myapp/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def bweb(request):
-    return HttpResponse("Hello, world!")
+    return HttpResponse("Hello, this is bweb view 完成")
